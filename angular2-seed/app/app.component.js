672d26ff6a58fb97975b6597c884080e5f375830
sumin <sumin@does.kr>
2016-06-21 14:00:04 +0900

    class style event binding

--- angular2-seed/app/app.component.js
+++ angular2-seed/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './courses.component', './authors.component'], function(exports_1, context_1) {
+System.register(['angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,30 +10,29 @@ System.register(['angular2/core', './courses.component', './authors.component'],
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, courses_component_1, authors_component_1;
+    var core_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (courses_component_1_1) {
-                courses_component_1 = courses_component_1_1;
-            },
-            function (authors_component_1_1) {
-                authors_component_1 = authors_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
-                    this.title = "Angular App";
-                    this.imgUrl = "http://www.naver.com/";
+                    this.isActive = true;
                 }
+                AppComponent.prototype.onDivClick = function () {
+                    console.log("Handled by Div");
+                };
+                AppComponent.prototype.onClick = function ($event) {
+                    $event.stopPropagation();
+                    console.log("Clicked", $event);
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "<h1>{{ title }}</h1>\n        <h1 [textContent]=\"title\"></h1>\n                <courses></courses>\n                <authors></authors>",
-                        directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent]
+                        template: "\n        <div (click)=\"onDivClick()\">\n            <button (click)=\"onClick($event)\">Submit</button>\n        </div>\n    "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- angular2-seed/app/app.component.ts
+++ angular2-seed/app/app.component.ts
@@ -1,16 +1,22 @@
 import {Component} from 'angular2/core';
-import {CoursesComponent} from './courses.component';
-import {AuthorsComponent} from './authors.component';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>{{ title }}</h1>
-        <h1 [textContent]="title"></h1>
-                <courses></courses>
-                <authors></authors>`,
-    directives: [CoursesComponent, AuthorsComponent]
+    template: `
+        <div (click)="onDivClick()">
+            <button (click)="onClick($event)">Submit</button>
+        </div>
+    `
 })
 export class AppComponent {
-    title = "Angular App";
-    imgUrl = "http://www.naver.com/";
+    isActive = true;
+
+    onDivClick(){
+        console.log("Handled by Div");
+    }
+
+    onClick($event){
+      $event.stopPropagation();
+        console.log("Clicked", $event);
+    }
 }
