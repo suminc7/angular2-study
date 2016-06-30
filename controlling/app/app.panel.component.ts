cb0b072f2341159fbd77423a30be102d77d46656
sumin <sumin@does.kr>
2016-06-30 16:53:45 +0900

    delete js and map

---
+++ controlling/app/app.panel.component.ts
@@ -0,0 +1,19 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <ul>
+            <li>Title: {{ task.title }}</li>
+            <li>Assigned to: {{ task.assignee?.name }}</li>  
+</ul>
+    `
+})
+export class AppComponent {
+    task = {
+        title: 'Review applications',
+        assignee: {
+                name:null
+        }
+    }
+}
---
+++ controlling/app/bootstrap.panel.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'bs-panel',
+    template: `
+        <div class="panel panel-default">
+            <div class="panel-heading"></div>
+            <div class="panel-body"></div>
+        </div>
+    `
+})
+
+export class BootstrapPanel {
+    
+}
