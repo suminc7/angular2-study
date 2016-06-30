fe7512dee929101dda85d5352ff3326be46c68bc
sumin <sumin@does.kr>
2016-06-30 18:16:23 +0900

    twitter zippy

---
+++ pages/zippy/app.component.ts
@@ -0,0 +1,19 @@
+import {Component} from 'angular2/core';
+import {ZippyComponent} from "./zippy.component";
+
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <zippy title="Who can see my stuff?">
+            Content of who can see my stuff
+        </zippy>
+        <zippy title="Who can contact me?">
+            Content of who can cantact me
+        </zippy>    
+    `,
+    directives: [ZippyComponent]
+})
+export class AppComponent {
+
+}
